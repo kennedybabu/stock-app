55bd1a05c12ef8c5bc0c289f92fb73e450372615
kennedybabu <kennedynjogu8@gmail.com>
2022-08-14 23:15:43 +0300

    feature: render graph depending on the user input

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,31 +1,47 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {BsCalendar3} from "react-icons/bs"
 import {Line} from "react-chartjs-2"
-
+import StockGraph from './StockGraph'
 
 const Graph = ({stock}) => {
 
-  // const [selectedQuote, setSelectedQuote] = useState(stock)  
+  
+  const data = {
+    labels: stock && stock.dataset && stock.dataset.data.map(item => item[0]),
+    datasets : [
+      {
+        label: stock && stock.dataset && stock.dataset.name,
+        data : stock && stock.dataset && stock.dataset.data.map(item => item[1])
+      }
+    ]
+  } 
 
-  // const labelsArr = stock.dataset.data
-
+  console.log(data) 
+ 
   return (
     <div className='w-full'>
-        {/* container */}     
-        <div className='w-full px-[21px] pt-[22px]'>
-            <div className='w-full flex justify-between'>
-                <p className='text-[22px] text-[#51459e] font-[500]'></p>
-                <div className='flex items-center'>
-                    <BsCalendar3 className='text-[#51459e] text-[32px]'/>
-                    <button className='w-[290px] h-[50px] rounded-[10px] text-white text-[18px] font-[500] ml-[32px] bg-[#51459e]'>Jun 5,1998 - Dec 31, 2018</button>
-                </div>
-            </div>
-            <div className='wrapper w-full h-[82vh] mt-[27px]'>
-                
-            </div>
-        </div>
-    </div>
+    { stock && stock.dataset && stock.dataset.data ? 
+      <div className='w-full'>   
+      <div className='w-full px-[21px] pt-[22px]'>
+          <div className='w-full flex justify-between'>
+          { stock && stock.dataset && stock.dataset.data ?  <p className='text-[22px] text-[#51459e] font-[500]'>{stock.dataset.dataset_code}</p> : null }              
+              <div className='flex items-center'>
+                  <BsCalendar3 className='text-[#51459e] text-[32px]'/>
+                  {/* {stock.dataset.name} */}
+                  <button className='w-[290px] h-[50px] rounded-[10px] text-white text-[18px] font-[500] ml-[32px] bg-[#51459e]'>Jun 5,1998 - Dec 31, 2018</button>
+              </div>
+          </div>
+          <div className='wrapper w-full h-[82vh] mt-[27px]'>
+            <StockGraph />
+          </div>
+      </div>
+  </div> : <p> one</p>
+}
+</div>
+    
   )
 }
 
 export default Graph
+
+  
--- src/components/StockGraph.jsx
+++ src/components/StockGraph.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
+import { Line } from 'react-chartjs-2'
+import { Chart as ChartJS, CategoryScale, LinearScale, 
+  PointElement, LineElement, Title, Tooltip, Legend,
+  } from 'chart.js';
+  
+  ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+  );  
+  
 
-const StockGraph = (props) => {
-  return (
-    <div>
-        a graph
-    </div>
-  )
+function StockGraph({stock}) {
+  const data = {
+    labels: stock && stock.dataset && stock.dataset.data.map(item => item[0]),
+    datasets: [
+      {
+        label: stock && stock.dataset && stock.dataset.name,
+        data : stock && stock.dataset && stock.dataset.data.map(item => item[1])
+      }
+    ]    
+  } 
+    
+  return  <Line data={data}/>
+  
 }
+
+
+
 
 export default StockGraph
